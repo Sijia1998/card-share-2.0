05c05a2a7a3d505114433ded7b855959355250b5
Sijia1998 <894744332@qq.com>
2018-08-15 20:32:09 +0800

    swiper init

--- card_share/src/components/Main.vue
+++ card_share/src/components/Main.vue
@@ -1,67 +1,91 @@
 <template>
-  <div class="swiper-container">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide one">Slide 1</div>
-      <div class="swiper-slide two">Slide 2</div>
-      <div class="swiper-slide three">Slide 3</div>
-      <div class="swiper-slide four">
-        <p>扫描下方二维码</p>
-        <p>获取你的消费总结</p>
-      </div>
-    </div>
+  <div class="swiper">
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide class="table">
+        <div class="center">
+          <p>学期账单</p>
+          <p>点击查看</p>
+        </div>
+        <img src="" alt="" style="width:30px">
+      </swiper-slide>
+      <swiper-slide>I'm Slide 2</swiper-slide>
+      <swiper-slide>I'm Slide 3</swiper-slide>
+      <swiper-slide>I'm Slide 4</swiper-slide>
+      <swiper-slide>I'm Slide 5</swiper-slide>
+      <swiper-slide>I'm Slide 6</swiper-slide>
+      <swiper-slide>I'm Slide 7</swiper-slide>
+      <!-- Optional controls -->
+      <!-- <div class="swiper-pagination" slot="pagination"></div> -->
 
-    <div class="swiper-pagination"></div>
-    <!-- 滚动条 -->
-    <!-- <div class="swiper-scrollbar"></div> -->
+    </swiper>
   </div>
-
 </template>
 
 <script>
-import Swiper from 'swiper'
+import axios from 'axios'
 import dayjs from 'dayjs'
-import 'swiper/dist/css/swiper.min.css'
+import 'swiper/dist/css/swiper.css'
+
 
 export default {
-  components: {
-
-  },
-  props: {
-
-  },
   data() {
     return {
-
+      costTotal: 5800,
+      costRes: 66666,
+      swiperOption: {
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+        direction: 'vertical',
+        fade: 'true',
+        autoplay: false,
+        // speed: 1000,
+        height: window.innerHeight,
+        width: window.innerWidth
+      }
     }
   },
-  methods: {},
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper
+    }
+  },
+  methods: {
+    // getInit() {
+    //   let self = this;
+    //   let begindate = dayjs().format('2018-03-04');
+    //   let enddate = dayjs().format('2018-07-12')
+    //   axios.get('http://118.126.110.182:8002/api/getNewData', {
+    //     params: {
+    //       begindate,
+    //       enddate,
+    //     },
+    //     headers: {
+    //       'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //   })
+    //     .then((res) => {
+    //       console.log(res);
+
+    //     })
+    // }
+  },
   mounted() {
-    let mySwiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      //垂直滚动
-      direction: 'vertical',
-    })
+    console.log("this is current swiper instance object", this.swiper);
+    // this.getInit()
   }
 }
 </script>
 <style scoped>
-.swiper-container {
-    width: 300px;
-    height: 500px;
-    margin: 20px auto;
-}
-.one {
-    background-color: aqua;
-}
-.two {
-    background-color: blue;
-}
-.three {
-    background-color: gray;
-}
-.four {
-    background-color: cornflowerblue;
+.center {
+    /* text-align: center; */
+    background: aqua;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 50%;
+    height: 50%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
 }
 </style>
